12be65320d8c47df55e5720fa6d21d6b5fddfa69
chethanm933/chethanm933 <chethanm933@gmail.com>
2022-01-01 13:27:26 +0530

    Name Validation

--- com/useregistration/UserRegistration.java
+++ com/useregistration/UserRegistration.java
@@ -1,14 +1,10 @@
 package com.useregistration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.junit.jupiter.api.Test;
 
 public class UserRegistration {
 	
