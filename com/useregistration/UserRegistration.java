112dd0ce8a25ac47412856d8ed7d86fd7b8b4ea8
chethanm933/chethanm933 <chethanm933@gmail.com>
2022-01-01 18:37:34 +0530

    Try catch exception User Details

--- com/useregistration/UserRegistration.java
+++ com/useregistration/UserRegistration.java
@@ -15,94 +15,110 @@ public class UserRegistration {
 	static Scanner sc = new Scanner(System.in);
 	
 	 public static void main(String[] args) {
-		 
-		 UserRegistration userreg = new UserRegistration();
-		 
-		 System.out.println("enter your firstname");
-		 String name = sc.next();
-		 userreg.name(name);
 		
-		 System.out.println("enter your lastname");
-		 String lastName = sc.next();
-		 userreg.lastName(lastName);
-		 
-		 System.out.println("enter your phone number");
-		 String phone = sc.next();
-		 userreg.phone(phone);
-		 
-		 System.out.println("enter your email");
-		 String email = sc.next();
-		 userreg.email(email);
-		 
-		 System.out.println("enter your firstname");
-		 String password = sc.next();
-		 userreg.password(password);
+		 System.out.print("Enter the first name : ");
+			try {
+				String name = sc.nextLine();
+				char val = name.charAt(20);
+				String namePattern = "^[A-Z]{1}[a-z]{2,9}$";
+				Pattern compileName = Pattern.compile(namePattern);
+				Matcher NameMatcher = compileName.matcher(name);
+				sc.close();
+				System.out.print(val);
+
+				if (NameMatcher.matches()) {
+					System.out.println(name + " is valid name.");
+				} else {
+					System.out.println(name + " is invalid name please try again.");
+				}
+			}
+
+			catch (StringIndexOutOfBoundsException e) {
+				System.out.println("StringIndexOutOfBoundsException");
+			}
+			
+			
+			System.out.print("Enter the last name : ");
+			try {
+				String lastName = sc.nextLine();
+				char val = lastName.charAt(20);
+				String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+				Pattern compileLastName = Pattern.compile(lastNamePattern);
+				Matcher lastNameMatcher = compileLastName.matcher(lastName);
+				sc.close();
+				System.out.print(val);
+
+				if (lastNameMatcher.matches()) {
+					System.out.println(lastName + " is valid ");
+				} else {
+					System.out.println(lastName + " is invalid try again");
+				}
+			}
+
+			catch (StringIndexOutOfBoundsException e) {
+				System.out.println("StringIndexOutOfBoundsException");
+			}
+
+			System.out.print("Enter the valid mail ID : ");
+			try {
+				
+				String email = sc.nextLine();
+				char val = email.charAt(20);
+				String emailPattern = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+				Pattern compileEmail = Pattern.compile(emailPattern);
+				Matcher emailMatcher = compileEmail.matcher(emailPattern);
+				sc.close();
+				System.out.print(val);
+
+				if (emailMatcher.matches()) {
+					System.out.println(emailPattern + " is valid ");
+				} else {
+					System.out.println(emailPattern + " is invalid please try again");
+				}
+			}
+
+			catch (StringIndexOutOfBoundsException e) {
+				System.out.println("StringIndexOutOfBoundsException");
+			}
+			
+		 System.out.print("Enter the phone number : ");
+			try {
+				String phone = sc.nextLine();
+				int val = Integer.parseInt("");
+				String phonePattern = "^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[\s 0-9]*$";
+				Pattern compilePhone = Pattern.compile(phonePattern);
+				Matcher phoneMatcher = compilePhone.matcher(phone);
+				sc.close();
+				System.out.print(val);
+
+				if (phoneMatcher.matches()) {
+					System.out.println(phone + " is valid ");
+				} else {
+					System.out.println(phone + " is invalid name.please try again.");
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Number format exception");
+			}
+			
+			System.out.print("Enter the password : ");
+			try {
+			String password = sc.nextLine();
+			String passwordPattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*#?&]{8,}$";
+			Pattern compilePassword = Pattern.compile(passwordPattern);
+			Matcher passwordMatcher = compilePassword.matcher(password);
+			
+			int val = Integer.parseInt("");
+			sc.close();
+			System.out.print(val);
+
+			if (passwordMatcher.matches()) {
+				System.out.println(password + " is valid ");
+			} else {
+				System.out.println(password + " is invalid please ty again.");
+			}
+		} catch (NumberFormatException e) {
+			System.out.println("Password  exception");
+		}
 	 }
 	 
-		public boolean name(String name) {
-		System.out.println("Enter a First Name");
-		name = sc.next();
-		String namePattern = "^[A-Z]{1}[a-z]{2,}$";
-		Pattern compileName = Pattern.compile(namePattern);
-		Matcher nameMatcher = compileName.matcher(name);
-		if(nameMatcher.matches())
-			return true;
-		else 
-			return false;
-		}
-		
-		public String lastName(String lastName) {
-		System.out.println("Enter a LastName");
-		lastName = sc.next();
-		String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
-		Pattern compileLastName = Pattern.compile(lastNamePattern);
-		Matcher lastNameMatcher = compileLastName.matcher(lastName);
-		
-		while(!lastNameMatcher.matches()) {
-			System.out.println("Invalid input try again :");
-			lastName = sc.next();
-			lastNameMatcher = compileLastName.matcher(lastName);
-			}
-				return lastName;
-		}
-		
-		
-		public boolean email(String email) {
-		System.out.println("Enter your emailid");
-		email = sc.next();
-		String emailPattern = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
-		Pattern compileEmail = Pattern.compile(emailPattern);
-		Matcher emailMatcher = compileEmail.matcher(email);
-		
-		if(emailMatcher.matches())
-			return true;
-		else 
-			return false;
-		}
-		
-		
-		public boolean phone(String phone) {
-		System.out.println("Enter a Phone Number");
-		phone = sc.next();
-		String phonePattern = "^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[\s 0-9]*$";
-		Pattern compilePhone = Pattern.compile(phonePattern);
-		Matcher phoneMatcher = compilePhone.matcher(phone);
-		if(phoneMatcher.matches())
-			return true;
-		else 
-			return false;
-		}
-		
-		public boolean password(String password) {
-		System.out.println("Enter a Password");
-		password = sc.next();
-		String passwordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
-		Pattern compilePassword = Pattern.compile(passwordPattern);
-		Matcher passwordMatcher = compilePassword.matcher(password);
-		
-		if(passwordMatcher.matches())
-			return true;
-		else 
-			return false;
-		}
 }
